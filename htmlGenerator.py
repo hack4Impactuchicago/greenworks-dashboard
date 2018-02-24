0475b0ea6dd4b12bc55dbcca61d0d75f68837808
Laya Gollapudi <layagollapudi@Layas-MacBook-Pro.local>
2018-02-24 17:22:32 -0600

    committing changes

--- htmlGenerator.py
+++ htmlGenerator.py
@@ -1,11 +1,63 @@
 import json
+import os
 import csvToDictionary as reader
-from flask import Flask, request, render_template, Markup
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
-@app.route('/')
+#def dictToHtml(opts):
+#    return render_template(.., data=json.dumps(opts))
+
+UPLOAD_FOLDER = '~/layagollapudi/Desktop'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods = ['GET'])
 def landing():
     return render_template("default.html");
+
+
+#@app.route('/', methods = ['POST'])
+# def result():
+#   if request.method == 'POST':
+#        #function for saving the information in server
+#        #use jquery
+#       result = request.form;
+#       return render_template("default.html",result = result);
+
+##for now just submitting data via postman, need to edit later to render edits
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files)
+        # check if the post request has the file part
+        if 'csv' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['csv']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print(filename)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 #@app.route('/')
 #def view():
@@ -16,8 +68,7 @@ def landing():
 
 # FLASK_APP=htmlGenerator.py flask run
 
-#def dictToHtml(opts):
-    #return render_template('_low_carbon_commute.html', data=json.dumps(opts))
+
 
 if __name__ == "__main__":
     app.run()
